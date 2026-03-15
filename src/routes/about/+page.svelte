6bdcce66c59c536e7e9e1cfbeab305fc369d5935
runningonsideprojects <runningonsideprojects@gmail.com>
2026-03-15 12:17:11 -0400

    adding about page with svelte configuration

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<h1>About</h1>
+<p>PhlartMap provides a map display of former and current Public Art displays around Philadelphia. Our interface integrates an API of a Philadelphia Public Art index curated and developed by <a href="https://www.philart.net/chrispurdom/">Christopher William Purdom</a> on <a href="https://philart.net">philart.net</a></p>
+<h2>Why is public art important?</h2>
+<p>If you need to ask this question then this site is not for you.</p> 
+<p>Made with love by Alison, AJ, Christina, Emily, Zepher</p>
