60a8ab4863688b6559abf6b814fe6a9b11cf20f2
ajstensland <35818081+ajstensland@users.noreply.github.com>
2026-03-15 17:09:21 -0400

    Move zoom control to bottom left

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -59,6 +59,7 @@
 			popupAnchor: popupAnchor
 		});
 		var map = L.map('map').setView([39.962125, -75.140675], 15);
+		map.zoomControl.setPosition("bottomleft");
 		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			maxZoom: 19,
 			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
