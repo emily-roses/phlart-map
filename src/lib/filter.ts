d7069ad6c2c8347ebca9cb8216cafbd363938fd2
runningonsideprojects <runningonsideprojects@gmail.com>
2026-03-15 15:02:08 -0400

    got filter to work yay

--- src/lib/filter.ts
+++ src/lib/filter.ts
@@ -1,13 +1,14 @@
 import type { Head, Art, ArtResponse} from './api.ts';
 import { getData } from './api.ts';
-const filterAttribute = 'landmark';
+const filterAttribute = "landmark";
 
 const artDataResponse: ArtResponse = await getData();
 const artDataBody = artDataResponse['body'];
 export const filter = (filterAttribute: string, artDataBody: Art[] ) => {
-    artDataBody['art'].filter((art: Art) => {
-        console.log(art);
-    })
+    return artDataBody['art'].filter((art: Art) => {
+        return (filterAttribute in art);
+    });
 };
 
 export const filterTesting = filter(filterAttribute, artDataBody);
+console.log(filterTesting);
