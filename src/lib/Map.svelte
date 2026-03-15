c10855fd29476ee7c5ee13bea7adc5d9b824bf08
Zephyr Prusinski <zephyr@zephnet.biz>
2026-03-15 15:03:10 -0400

    custom markers and popups

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -8,15 +8,40 @@
 <script>
   import { onMount } from 'svelte';
   onMount(async () => {
-    console.log(window.innerWidth);
     const L = await import('leaflet')
+
+    const artJson = await fetch('https://www.philart.net/api/art.json', {
+      method: 'GET'
+    })
+    console.log(artJson);
     // indy hall location
+    var architecture = L.icon({
+	iconUrl: 'icons/architecture.png',
+	iconSize: [38, 60],
+	iconAnchor: [17, 60],
+	popupAnchor: [-3, -76],
+    });
+    var art = L.icon({
+	iconUrl: 'icons/art.png',
+	iconSize: [38, 60],
+	iconAnchor: [17, 60],
+	popupAnchor: [0,-60],
+    });
+    var landmark = L.icon({
+	iconUrl: 'icons/landmark.png',
+	iconSize: [38, 60],
+	iconAnchor: [17, 60],
+	popupAnchor: [-3, -76],
+    });
     var map = L.map('map').setView([39.962125, -75.140675], 15);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    L.marker([39.96164917971281, -75.14149386089039]).addTo(map);
+    let marker = L.marker([39.96164917971281, -75.14149386089039], {icon: art}); 
+    let popup = L.popup().setContent("I am a standalone popup.");
+    marker.bindPopup(popup).openPopup();
+    marker.addTo(map);
   });
 </script>
 
