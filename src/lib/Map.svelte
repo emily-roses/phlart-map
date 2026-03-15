728ff5cf50b2f92fe68910ccdaa8a831392864d4
Zephyr Prusinski <zephyr@zephnet.biz>
2026-03-15 15:30:56 -0400

    added placeholder text for popups

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -8,7 +8,7 @@
   onMount(async () => {
     const L = await import('leaflet')
 
-    const artJson = await fetch('https://www.philart.net/api/art.json', {
+    const artJson = await fetch('https://www.philart.net/api/landmarks.json', {
       method: 'GET'
     })
     console.log(artJson);
@@ -17,27 +17,36 @@
 	iconUrl: 'icons/architecture.png',
 	iconSize: [38, 60],
 	iconAnchor: [17, 60],
-	popupAnchor: [-3, -76],
-    });
-    var art = L.icon({
-	iconUrl: 'icons/art.png',
-	iconSize: [38, 60],
-	iconAnchor: [17, 60],
 	popupAnchor: [0,-60],
     });
     var landmark = L.icon({
 	iconUrl: 'icons/landmark.png',
 	iconSize: [38, 60],
 	iconAnchor: [17, 60],
-	popupAnchor: [-3, -76],
+	popupAnchor: [0,-60],
     });
     var map = L.map('map').setView([39.962125, -75.140675], 15);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    let marker = L.marker([39.96164917971281, -75.14149386089039], {icon: art}); 
-    let popup = L.popup().setContent("I am a standalone popup.");
+    let marker = L.marker([39.96164917971281, -75.14149386089039], {icon: landmark}); 
+    let list = document.createElement('ul');
+    let contentJson = {
+      image: '/image.jpg',
+      name: 'Guy Person',
+      address: '21 Jump Street',
+    }
+    let listitem, listitemTextContent;
+    for (const key in contentJson) {
+      if (contentJson.hasOwnProperty(key)) {
+	listitem = document.createElement('li');
+	listitemTextContent = document.createTextNode(contentJson[key]);
+	listitem.appendChild(listitemTextContent);
+	list.appendChild(listitem);
+      }
+    }
+    let popup = L.popup().setContent(list);
     marker.bindPopup(popup).openPopup();
     marker.addTo(map);
   });
