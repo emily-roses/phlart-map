684eb378f845ccfbc10aff5cb7675cbbdb45110b
runningonsideprojects <runningonsideprojects@gmail.com>
2026-03-15 12:38:13 -0400

    about page is donezo

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,9 @@
+
 <h1>About</h1>
-<p>PhlartMap provides a map display of former and current Public Art displays around Philadelphia. Our interface integrates an API of a Philadelphia Public Art index curated and developed by <a href="https://www.philart.net/chrispurdom/">Christopher William Purdom</a> on <a href="https://philart.net">philart.net</a></p>
+<p>PhlartMap provides a map display of former and current Public Art displays around Philadelphia. Our interface integrates an API of a Philadelphia Public Art index curated and developed by <a target= "_blank" href="https://www.philart.net/chrispurdom/">Christopher William Purdom</a> on <a target="_blank" href="https://philart.net">philart.net</a></p>
+<h2>How do I use this tool?</h2>
+<p>Enter a Philadelphia address of your choosing or traverse through the city on our map. Hover on each pin to learn about what art is featured in that location.</p>
 <h2>Why is public art important?</h2>
 <p>If you need to ask this question then this site is not for you.</p> 
-<p>Made with love by Alison, AJ, Christina, Emily, Zepher</p>
+<p style="font-weight: bold; color: #505050; margin-top: 2em;">Made with love by Alison, AJ, Christina, Emily, and Zephyr</p>
+
