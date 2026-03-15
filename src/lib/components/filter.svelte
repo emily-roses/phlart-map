8e4231eac6b2c6c13f31efe2a091a181bd6570e0
runningonsideprojects <runningonsideprojects@gmail.com>
2026-03-15 15:29:29 -0400

    starting filter

---
+++ src/lib/components/filter.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    const mainFilterTypes = ["landmark", "architecture", "exhibits"];
+</script>
+{#each mainFilterTypes as mainFilter}
+<input id={mainFilter} type="checkbox">
+<label for={mainFilter}>{mainFilter}</label>
+{/each}
