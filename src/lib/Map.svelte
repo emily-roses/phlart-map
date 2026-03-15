01aaae1765cb0e197546c74f2d88025f2a452574
emily <emilykbonar@gmail.com>
2026-03-15 16:31:22 -0400

    Extract addressinput

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,75 +1,74 @@
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-     crossorigin=""/>
-<div id="map">
-  <div class="nav-header">
-    <input class="search-box" type="text" placeholder="enter an address..." style="z-index: 1000; position: relative;" id="search">
-    <Filter />
-  </div>
-</div>
-
 <script>
-	import Filter from "$lib/components/Filter.svelte"
-  import { onMount } from 'svelte';
-  onMount(async () => {
-    const L = await import('leaflet')
+	import AddressInput from './components/AddressInput.svelte';
+	import Filter from '$lib/components/Filter.svelte';
+	import { onMount } from 'svelte';
+	onMount(async () => {
+		const L = await import('leaflet');
 
-    const artJson = await fetch('https://www.philart.net/api/landmarks.json', {
-      method: 'GET'
-    })
-    console.log(artJson);
-    // indy hall location
-    var architecture = L.icon({
-	iconUrl: 'icons/architecture.png',
-	iconSize: [38, 60],
-	iconAnchor: [17, 60],
-	popupAnchor: [0,-60],
-    });
-    var landmark = L.icon({
-	iconUrl: 'icons/landmark.png',
-	iconSize: [38, 60],
-	iconAnchor: [17, 60],
-	popupAnchor: [0,-60],
-    });
-    var map = L.map('map').setView([39.962125, -75.140675], 15);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    let marker = L.marker([39.96164917971281, -75.14149386089039], {icon: landmark}); 
-    let list = document.createElement('ul');
-    let contentJson = {
-      image: '/image.jpg',
-      name: 'Guy Person',
-      address: '21 Jump Street',
-    }
-    let listitem, listitemTextContent;
-    for (const key in contentJson) {
-      if (contentJson.hasOwnProperty(key)) {
-	listitem = document.createElement('li');
-	listitemTextContent = document.createTextNode(contentJson[key]);
-	listitem.appendChild(listitemTextContent);
-	list.appendChild(listitem);
-      }
-    }
-    let popup = L.popup().setContent(list);
-    marker.bindPopup(popup).openPopup();
-    marker.addTo(map);
-  });
+		const artJson = await fetch('https://www.philart.net/api/landmarks.json', {
+			method: 'GET'
+		});
+		console.log(artJson);
+		// indy hall location
+		var architecture = L.icon({
+			iconUrl: 'icons/architecture.png',
+			iconSize: [38, 60],
+			iconAnchor: [17, 60],
+			popupAnchor: [0, -60]
+		});
+		var landmark = L.icon({
+			iconUrl: 'icons/landmark.png',
+			iconSize: [38, 60],
+			iconAnchor: [17, 60],
+			popupAnchor: [0, -60]
+		});
+		var map = L.map('map').setView([39.962125, -75.140675], 15);
+		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 19,
+			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		}).addTo(map);
+		let marker = L.marker([39.96164917971281, -75.14149386089039], { icon: landmark });
+		let list = document.createElement('ul');
+		let contentJson = {
+			image: '/image.jpg',
+			name: 'Guy Person',
+			address: '21 Jump Street'
+		};
+		let listitem, listitemTextContent;
+		for (const key in contentJson) {
+			if (contentJson.hasOwnProperty(key)) {
+				listitem = document.createElement('li');
+				listitemTextContent = document.createTextNode(contentJson[key]);
+				listitem.appendChild(listitemTextContent);
+				list.appendChild(listitem);
+			}
+		}
+		let popup = L.popup().setContent(list);
+		marker.bindPopup(popup).openPopup();
+		marker.addTo(map);
+	});
 </script>
 
+<link
+	rel="stylesheet"
+	href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+	integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+	crossorigin=""
+/>
+<div id="map">
+	<div class="nav-header">
+		<AddressInput></AddressInput>
+		<Filter />
+	</div>
+</div>
+
 <style>
-  #map {height: 100vh; }
-  .search-box{
-    width: 20%;
-    min-width: 200px;
-    margin-top: 1em;
-    margin-left: 5em;
-    height: 20px;
-  }
-  .nav-header{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
+	#map {
+		height: 100vh;
+	}
+	.nav-header {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
 </style>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,7 +3,7 @@ import { addrToCordinates } from '$lib/utils/geocode';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
-	console.log(await getAllArt());
+	console.log(getAllArt());
 	console.log(await addrToCordinates('32 evans st, warminster, pa 18974'));
 };
 
