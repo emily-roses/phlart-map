047fc759dc0ec661ee512ae3c0d54be67ff667b8
Emily Rose <hello@fauxtrots.com>
2026-03-16 15:48:53 -0400

    Remove some broken filtering code

--- src/lib/utils/filter.ts
+++ src/lib/utils/filter.ts
@@ -1,14 +1,8 @@
-import type { Head, Art, ArtResponse } from './api.ts';
-import { getData } from './api.ts';
-const filterAttributes = ["landmark"];
+import type { Art } from './api.ts';
+const filterAttributes = ['landmark'];
 
-const artDataResponse: ArtResponse = await getData();
-const artDataBody = artDataResponse['body'];
-export const filter = (filterAttributes: string[], artDataBody: Art[] ) => {
-    return artDataBody['art'].filter((art: Art) => {
-        return filterAttributes.every(attribute => attribute in art);
-    });
+export const filter = (filterAttributes: string[], artDataBody: Art[]) => {
+	return artDataBody.filter((art: Art) => {
+		return filterAttributes.every((attribute) => attribute in art);
+	});
 };
-
-export const filterTesting = filter(filterAttributes, artDataBody);
-console.log(filterTesting);
